5932968dd6c89e3de8df2fc26642dc02bcd2ef03
Rreuben <jreuben07@gmail.com>
2018-05-15 16:09:36 +0300

    Complete website structure with added JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
+// User Interface
+
 function openNav() {
   document.getElementById('myNav').style.width = "100%";
 }
 function closeNav() {
   document.getElementById('myNav').style.width = "0%";
 }
+function ClearField() {
+  document.getElementById("emailBox").value = "";
+}
+$(document).ready(function() {
+  $('#subscribe').click(function() {
+    alert('Thank You For Subscribing!');
+
+    ClearField();
+  });
+});
